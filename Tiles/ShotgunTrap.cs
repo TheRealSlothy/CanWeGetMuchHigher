06077fa7dcd0fc262aa22706d9c7cf9c7496ee30
Slothy <105455001+TheRealSlothy@users.noreply.github.com>
2026-01-28 14:18:29 +0000

    Add files via upload

---
+++ Tiles/ShotgunTrap.cs
@@ -0,0 +1,39 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+
+namespace CanWeGetMuchHigher.Content.Tiles
+{
+    internal class ShotgunTrap : ModTile
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileID.Sets.DisableSmartCursor[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16 };
+
+            TileObjectData.newTile.HookPostPlaceMyPlayer = ModContent.GetInstance<Tiles.ShotgunTrapEntity>().Generic_HookPostPlaceMyPlayer;
+
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(200, 200, 200), CreateMapEntryName());
+
+            RegisterItemDrop(ModContent.ItemType<Items.Placeables.ShotgunTrap>());
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            ModContent.GetInstance<Tiles.ShotgunTrapEntity>().Kill(i, j);
+        }
+    }
+}
