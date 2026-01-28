628ab82fe69d18ec75f7e278f74523d11129d16e
Slothy <105455001+TheRealSlothy@users.noreply.github.com>
2026-01-28 14:16:17 +0000

    Add files via upload

---
+++ CanWeGetMuchHigher.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.UI;
+using Microsoft.Xna.Framework;
+
+
+namespace CanWeGetMuchHigher
+{
+	public class CanWeGetMuchHigher : Mod
+	{
+
+    }
+}
