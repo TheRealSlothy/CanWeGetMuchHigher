a0242f6bd90267627685b0ad4bf45d71ea45f4fe
Slothy <105455001+TheRealSlothy@users.noreply.github.com>
2026-01-28 18:24:14 +0000

    Update BossDownedSystem.cs

--- Content/Systems/BossDownedSystem.cs
+++ Content/Systems/BossDownedSystem.cs
@@ -1,1 +1,36 @@
+using Terraria.ModLoader;
 
+namespace CanWeGetMuchHigher.Content.Systems
+{
+	public class BossDownedSystem : ModSystem
+	{
+		public static bool TrapperbossDowned = false;
+		
+		
+		public override void ClearWorld()
+		{
+			TrapperbossDowned = false;
+		}
+		public override void SaveWorldData(TagCompound tag)
+		{
+			tag["TrapperbossDowned"] = TrapperbossDowned;
+		}
+		public override void LoadWorldData(TagCompound tag)
+		{
+			TrapperbossDowned = tag.GetBool("TrapperbossDowned");
+		}
+
+		public override void NotSend(BinaryWriter writer)
+		{
+			var flags = new BitsByte();
+			flags[0] = TrapperbossDowned;
+			writer.Write(flags);
+
+		}
+		public override void NetReceive(BinaryReader reader)
+		{
+			BitsByte flags = reader.ReadByte();
+			TrapperbossDowned = flags[0];
+		}
+	}
+}
